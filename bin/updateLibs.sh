c53bb9024e29c16ebbfecd508f22c1edfb558d87
Simon Heimler <mail@simon-heimler.de>
2023-12-30 16:49:09 +0100

    Remove jzz dependency for MIDI recording

--- bin/updateLibs.sh
+++ bin/updateLibs.sh
@@ -6,5 +6,5 @@ cat node_modules/bootstrap/dist/css/bootstrap.min.css.map >> ./web/lib/bootstrap
 cat node_modules/bootstrap/dist/js/bootstrap.bundle.min.js >> ./web/lib/bootstrap.bundle.min.js
 
 cat node_modules/webmidi/dist/iife/webmidi.iife.min.js >> ./web/lib/webmidi.iife.min.js
-cat node_modules/jzz/javascript/JZZ.js >> ./web/lib/JZZ.js
-cat node_modules/jzz-midi-smf/javascript/JZZ.midi.SMF.js >> ./web/lib/JZZ.midi.SMF.js
+# cat node_modules/jzz/javascript/JZZ.js >> ./web/lib/JZZ.js
+# cat node_modules/jzz-midi-smf/javascript/JZZ.midi.SMF.js >> ./web/lib/JZZ.midi.SMF.js
